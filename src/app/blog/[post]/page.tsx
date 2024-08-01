df04bf9d4559f8a128e31169687c97bc36865284
evan-mick <evsammick@gmail.com>
2024-08-01 01:16:51 -0700

    another attempt

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -1,6 +1,7 @@
 import { BlogPostMetaData } from "@/constants/types";
 import fs from "fs";
 import matter from "gray-matter"
+import Image from "next/image";
 import { HTMLAttributes, ReactNode } from "react";
 import ReactMarkdown from 'react-markdown'
 
@@ -22,7 +23,7 @@ export default function Page({ params }: { params: {post: string} }) {
         {post.data.show ? 
         <section className="m-20 p-10 rounded-md shadow-lg h-full block">
             <h1 className="text-6xl mb-10">{post.data.title}</h1>
-            <img className="w-full object-scale-down md:object-cover"
+            <Image className="w-full object-scale-down md:object-cover"
                         src={post.data.img}
                         alt="Photo"
                         width={100}
