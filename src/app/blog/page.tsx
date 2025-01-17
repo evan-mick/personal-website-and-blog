e20f6f477cf9a4a937441d023cb446d84cf10eec
evan-mick <evsammick@gmail.com>
2025-01-17 05:05:03 +0000

    stuff

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,17 +8,17 @@ import fs from "fs";
 import matter from "gray-matter"
 
 // thx https://www.youtube.com/watch?v=Hiabp1GY8fA
-const getPostsData = (): BlogPostMetaData[] => {
+const getPostsData = (file_path: string): BlogPostMetaData[] => {
 
-    const folder = "src/blog_posts";
+    const folder = file_path;
     const files = fs.readdirSync(folder);
     const markdownPosts = files
-                            .filter((file) => file.endsWith(".md"))
-                            .filter((filename) => {
-                                const fileContents = fs.readFileSync(`${folder}/${filename}`, "utf-8");
-                                const matterResult = matter(fileContents);
-                                return matterResult.data.show; 
-                            });
+        .filter((file) => file.endsWith(".md"))
+        .filter((filename) => {
+            const fileContents = fs.readFileSync(`${folder}/${filename}`, "utf-8");
+            const matterResult = matter(fileContents);
+            return matterResult.data.show;
+        });
 
     const posts: BlogPostMetaData[] = markdownPosts.map((filename) => {
         const fileContents = fs.readFileSync(`${folder}/${filename}`, "utf-8");
@@ -33,21 +33,19 @@ const getPostsData = (): BlogPostMetaData[] => {
         };
     });
 
-    
-    return posts; 
+
+    return posts;
 }
 
 
 export default function Page() {
 
-    const blogs: ProjectLinkHolder[] = getPostsData().map((meta) => {
-        console.log(meta.url);
+    const blogs: ProjectLinkHolder[] = getPostsData("src/blog_posts").map((meta) => {
         return {
             linkTo: meta.url,
             photoLink: meta.img,
             desc: meta.description,
             title: meta.name,
-            
         }
     });
 
@@ -55,7 +53,7 @@ export default function Page() {
         <section className="sm:m-20 p-10 rounded-md shadow-lg h-full bg-white">
             <h4 className="text-5xl text-center md:text-left pb-12">Blog</h4>
             <p className="pb-12">Like any man in tech/gamedev, I have projects, thoughts, opinions, etc. Sometimes *too* many. Here's my place to express them, read if you'd like. </p>
-            <ProjectDisplayHolder data={blogs}/>
+            <ProjectDisplayHolder data={blogs} />
         </section>
     )
 }
