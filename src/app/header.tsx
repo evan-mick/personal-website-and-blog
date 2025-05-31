99e173be2269991295fc26ec23cef54c97644cda
evan <evsammick@gmail.com>
2025-05-31 16:47:41 -0400

    linked in + github links

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,55 +1,73 @@
-
 // https://blog.logrocket.com/how-create-multilevel-dropdown-menu-react/
 
-
 export const menuItems = [
-    {
-      title: 'Home',
-      url: '/',
-    },
-    /*{
-      title: 'Blog',
-      url: '/blog',
-    },*/
-    {
-      title: 'Projects',
-      url: '/projects',
-    },
-    {
-      title: 'Blog',
-      url: '/blog',
-    },
-    /*{
-      title: 'Music',
-      url: '/music',
-    },*/
-    {
-      title: 'Contact',
-      url: '/contact',
-    },
+  {
+    title: "Home",
+    url: "/",
+  },
 
-  ];
-  
+  {
+    title: "Projects",
+    url: "/projects",
+  },
+  {
+    title: "Blog",
+    url: "/blog",
+  },
+  /*{
+    title: 'Music',
+    url: '/music',
+  },*/
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+];
 
 export default function Header() {
-    return (
-      <div className="shadow-lg relative z-0">
-        <nav>
-          <ul className="menus flex flex-wrap w-screen justify-center gap-x-10 md:gap-x-24">
-            {menuItems.map((menu, index) => {
-              return (
-                <li className="group menu-items w-18 my-6 text-center place-content-center content-center transition ease-out hover:scale-110" key={index}>
-                  {/* https://stackoverflow.com/questions/70906977/tailwind-underline-hover-animation */}
-                  <a href={menu.url}>
-                    {menu.title}
-                    <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-black"></span>
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-        </div>
-      );
+  return (
+    <div className="shadow-lg relative z-0">
+      <nav>
+        <ul className="menus flex flex-wrap w-screen justify-center gap-x-10 md:gap-x-24">
+          {menuItems.map((menu, index) => {
+            return (
+              <li
+                className="group menu-items w-18 my-6 text-center place-content-center content-center transition ease-out hover:scale-110"
+                key={index}
+              >
+                {/* https://stackoverflow.com/questions/70906977/tailwind-underline-hover-animation */}
+                <a href={menu.url}>
+                  {menu.title}
+                  <span className="block max-w-0 group-hover:max-w-full transition-all duration-200 h-0.5 bg-black">
+                  </span>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
 
+      <div className="flex justify-center space-x-4 pb-6 sm:pb-0 sm:absolute sm:right-8 sm:top-1/2 sm:-translate-y-1/2">
+        <a
+          href={"https://github.com/evan-mick"}
+        >
+          <img
+            className="w-12"
+            src={"icons8-github.svg"}
+            alt="Photo"
+          />
+        </a>
+        <a
+          href={"https://www.linkedin.com/in/evan-mickelson-582586190/"}
+        >
+          <img
+            className="w-12"
+            src={"linkedin-svgrepo-com.svg"}
+            alt="LinkedIn Link"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
+
--- src/components/ProjectDisplay.tsx
+++ src/components/ProjectDisplay.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+"use client";
 
 import "../globals.css";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
@@ -8,28 +7,29 @@ import { useRouter } from "next/navigation";
 import { RedirectType } from "next/dist/client/components/redirect";
 import { ProjectLinkHolder } from "@/constants/types";
 
-
-
 export default function ProjectDisplay(props: ProjectLinkHolder) {
-
     const router = useRouter();
     function onAnyClick() {
-        console.log("clicked " + props.linkTo); 
+        console.log("clicked " + props.linkTo);
 
         router.push(props.linkTo);
 
         //redirect(props.linkTo, RedirectType.replace);
-
     }
 
     console.log(props.linkTo);
 
     return (
         <div className="h-76 w-96 shadow-lg rounded-lg hover:outline transition ease-out hover:scale-105">
-            
-            <button onClick={() => onAnyClick()} className="flex-col justify-center align-middle items-center ">
-                {props.title == undefined ? <></> :<h1 className="text-l mb-2 mt-2">{props.title}</h1>}
-                <img className="w-96 md:h-48 object-scale-down md:object-cover"
+            <button
+                onClick={() => onAnyClick()}
+                className="flex-col justify-center align-middle items-center "
+            >
+                {props.title == undefined
+                    ? <></>
+                    : <h1 className="text-l mb-2 mt-2">{props.title}</h1>}
+                <img
+                    className="w-96 md:h-48 object-scale-down md:object-cover"
                     src={props.photoLink}
                     alt="Photo"
                     width={100}
@@ -40,7 +40,5 @@ export default function ProjectDisplay(props: ProjectLinkHolder) {
             </button>
         </div>
     );
-
-
-    
 }
+
